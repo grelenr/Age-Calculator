c89571559c4e967d73f6c50753214e676e7318c3
Rebekah Grelen <rebekahgrelen@Rebekahs-Air.attlocal.net>
2020-07-29 21:36:06 -0500

    Changed the image, will need to resize it at some point

--- scratch.py
+++ scratch.py
@@ -11,51 +11,51 @@ from PIL import ImageTk, Image
 root = Tk()
 root.geometry("700x500")
 root.title("Age Calculator")
-photo = ImageTk.PhotoImage(Image.open("oldcalculator.jpg"))
-myimage = Label(root, image=photo)
-myimage.grid(row=0, column=1)
-
-#Originally I had 2 functions, calculateAge() and calculateZodiac(). I tried running them consecutively, it did not work.
-#Overall I just simplified and made everything more unified (ex. months are all ints now rather than strings for Zodiac)
-#Removed the zodiac month/day "input" code. It was making the user input info in command line rather than the app window
-#Added monthEntry.get() as a value for the variable 'month' and dayEntry.get() for 'day'
-
+#photo = ImageTk.PhotoImage(Image.open("/Users/rebekahgrelen/Desktop/GitProjects/Age-Calculator/newcalculator.jpg"))
+#myimage = Label(root, image=photo)
+#myimage.grid(row=0, column=1)
+"""
+Originally I had 2 functions, calculateAge() and calculateZodiac(). I tried running them consecutively, it did not work.
+Overall I just simplified and made everything more unified (ex. months are all ints now rather than strings for Zodiac)
+Removed the zodiac month/day "input" code. It was making the user input info in command line rather than the app window
+Added monthEntry.get() as a value for the variable 'month' and dayEntry.get() for 'day'
+"""
 
 def calculateAge():
-    today = date.today()
-    birthDate = date(int(yearEntry.get()),
-    int(monthEntry.get()), int(dayEntry.get()))
-    age = today.year - birthDate.year - ((today.month, today. day) < (birthDate.month, birthDate.day))
-    month = int(monthEntry.get())
-    day = int(dayEntry.get())
-    if month == 12:
-        astro_sign = 'Sagittarius' if (day < 22) else 'capricorn'
-    elif month == 1:
-        astro_sign = 'Capricorn' if (day < 20) else 'aquarius'
-    elif month == 2:
-        astro_sign = 'Aquarius' if (day < 19) else 'pisces'
-    elif month == 3:
-        astro_sign = 'Pisces' if (day < 21) else 'aries'
-    elif month == 4:
-        astro_sign = 'Aries' if (day < 20) else 'taurus'
-    elif month == 5:
-        astro_sign = 'Taurus' if (day < 21) else 'gemini'
-    elif month == 6:
-        astro_sign = 'Gemini' if (day < 21) else 'cancer'
-    elif month == 7:
-        astro_sign = 'Cancer' if (day < 23) else 'leo'
-    elif month == 8:
-        astro_sign = 'Leo' if (day < 23) else 'virgo'
-    elif month == 9:
-        astro_sign = 'Virgo' if (day < 23) else 'libra'
-    elif month == 10:
-        astro_sign = 'Libra' if (day < 23) else 'scorpio'
-    elif month == 11:
-        astro_sign = 'scorpio' if (day < 22) else 'sagittarius'
+today = date.today()
+birthDate = date(int(yearEntry.get()),
+int(monthEntry.get()), int(dayEntry.get()))
+age = today.year - birthDate.year - ((today.month, today. day) < (birthDate.month, birthDate.day))
+month = int(monthEntry.get())
+day = int(dayEntry.get())
+if month == 12:
+    astro_sign = 'Sagittarius' if (day < 22) else 'capricorn'
+elif month == 1:
+    astro_sign = 'Capricorn' if (day < 20) else 'aquarius'
+elif month == 2:
+    astro_sign = 'Aquarius' if (day < 19) else 'pisces'
+elif month == 3:
+    astro_sign = 'Pisces' if (day < 21) else 'aries'
+elif month == 4:
+    astro_sign = 'Aries' if (day < 20) else 'taurus'
+elif month == 5:
+    astro_sign = 'Taurus' if (day < 21) else 'gemini'
+elif month == 6:
+    astro_sign = 'Gemini' if (day < 21) else 'cancer'
+elif month == 7:
+    astro_sign = 'Cancer' if (day < 23) else 'leo'
+elif month == 8:
+    astro_sign = 'Leo' if (day < 23) else 'virgo'
+elif month == 9:
+    astro_sign = 'Virgo' if (day < 23) else 'libra'
+elif month == 10:
+    astro_sign = 'Libra' if (day < 23) else 'scorpio'
+elif month == 11:
+    astro_sign = 'scorpio' if (day < 22) else 'sagittarius'
 
-    # updated the button to include the Zodiac sign
+# updated the button to include the Zodiac sign
 
-    Label(text=f"{nameValue.get()} your age is {age} and your Zodiac sign is {astro_sign}").grid(row=6, column=1)
+Label(text=f"{nameValue.get()} your age is {age} and your Zodiac sign is {astro_sign}").grid(row=6, column=1)
 
 Label(text="Name").grid(row=1, column=0, padx=90)
 Label(text="Year").grid(row=2, column=0)
