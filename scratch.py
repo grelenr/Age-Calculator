e63dbf73c0de82a8479467820d092d5cfcde05f0
Rebekah Grelen <rebekahgrelen@Rebekahs-MacBook-Air.local>
2020-08-02 19:41:11 -0500

    Capitalized Zodiac Sign Names

--- scratch.py
+++ scratch.py
@@ -34,29 +34,29 @@ def calculateAge():
     day = int(dayEntry.get())
     age = today.year - birthDate.year - ((today.month, today.day) < (birthDate.month, birthDate.day))
     if month == 12:
-        astro_sign = 'Sagittarius' if (day < 22) else 'capricorn'
+        astro_sign = 'Sagittarius' if (day < 22) else 'Capricorn'
     elif month == 1:
-        astro_sign = 'Capricorn' if (day < 20) else 'aquarius'
+        astro_sign = 'Capricorn' if (day < 20) else 'Aquarius'
     elif month == 2:
-        astro_sign = 'Aquarius' if (day < 19) else 'pisces'
+        astro_sign = 'Aquarius' if (day < 19) else 'Pisces'
     elif month == 3:
-        astro_sign = 'Pisces' if (day < 21) else 'aries'
+        astro_sign = 'Pisces' if (day < 21) else 'Aries'
     elif month == 4:
-        astro_sign = 'Aries' if (day < 20) else 'taurus'
+        astro_sign = 'Aries' if (day < 20) else 'Taurus'
     elif month == 5:
-        astro_sign = 'Taurus' if (day < 21) else 'gemini'
+        astro_sign = 'Taurus' if (day < 21) else 'Gemini'
     elif month == 6:
-        astro_sign = 'Gemini' if (day < 21) else 'cancer'
+        astro_sign = 'Gemini' if (day < 21) else 'Cancer'
     elif month == 7:
-        astro_sign = 'Cancer' if (day < 23) else 'leo'
+        astro_sign = 'Cancer' if (day < 23) else 'Leo'
     elif month == 8:
-        astro_sign = 'Leo' if (day < 23) else 'virgo'
+        astro_sign = 'Leo' if (day < 23) else 'Virgo'
     elif month == 9:
-        astro_sign = 'Virgo' if (day < 23) else 'libra'
+        astro_sign = 'Virgo' if (day < 23) else 'Libra'
     elif month == 10:
-        astro_sign = 'Libra' if (day < 23) else 'scorpio'
+        astro_sign = 'Libra' if (day < 23) else 'Scorpio'
     elif month == 11:
-        astro_sign = 'scorpio' if (day < 22) else 'sagittarius'
+        astro_sign = 'Scorpio' if (day < 22) else 'Sagittarius'
 
 
     # updated the button to include the Zodiac sign
